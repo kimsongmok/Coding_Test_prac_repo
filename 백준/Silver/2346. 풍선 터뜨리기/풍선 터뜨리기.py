5f99e91bfa2cffd8f95d85f42a85249990cf9fa3
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2024-02-02 15:03:06 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 116 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,13 +1,15 @@
-from collections import deque
-n = int(input())
-q = deque(enumerate(map(int,input().split())))
-ans=[]
+import sys
+input = sys.stdin.readline
 
-while q:
-    idx,num = q.popleft()
-    ans.append(idx+1)
-    if num>0:
-        q.rotate(-(num-1))
-    elif num<0:
-        q.rotate(-num)
-print(' '.join(map(str,ans)))
+n = int(input())
+popping = list(enumerate(map(int, input().split())))
+idx = 0
+
+while popping:
+    ans = popping.pop(idx)
+    print(ans[0]+1,end= ' ')
+    
+    if ans[1] < 0 and popping:
+        idx = (idx + ans[1]) % len(popping)
+    elif ans[1] > 0 and popping:
+        idx = (idx + (ans[1]-1)) % len(popping)
