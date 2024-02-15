c2e9324e043082086d4a72274b9a42aeabe6b55a
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2024-02-15 14:54:23 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1000 ms, Memory: 161440 KB -BaekjoonHub

---
+++ 백준/Silver II/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m,k,x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [0] * (n+1)
+visited = [False] * (n+1)
+
+for _ in range(m):
+    u,v = map(int, input().split())
+    graph[u].append(v)
+
+def bfs(start):
+    answer = []
+    q = deque([start])
+    visited[start] = True
+    distance[start] = 0
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+                distance[i] = distance[now] + 1
+                if distance[i] == k:
+                    answer.append(i)
+    if len(answer) == 0:
+        print(-1)
+    else:
+        answer.sort()
+        for i in answer:
+            print(i)
+
+bfs(x)
