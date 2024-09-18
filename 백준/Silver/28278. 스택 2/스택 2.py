372a090c32b428e71cd320265f78e1df085a1528
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2024-09-18 23:21:12 +0900

    [Silver IV] Title: 스택 2, Time: 328 ms, Memory: 178900 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -3,24 +3,26 @@ input = sys.stdin.readline
 
 n = int(input())
 li = []
-for _ in range(n):
-    s =  input()
-    if s[0] == '1':
-        li.append(int(s[2:]))
-    elif s[0] == '2':
-        if len(li) == 0:
-            print(-1)
-        else:
-            print(li.pop())
-    elif s[0] == '3':
-        print(len(li))
-    elif s[0] == '4':
-        if len(li) == 0:
-            print(1)
-        else:
-            print(0)
-    elif s[0] == '5':
-        if len(li) == 0:
-            print(-1)
-        else:
-            print(li[-1])
+
+for i in range(n):
+  s = input()
+  if s[0] == '1':
+    li.append(int(s[2:]))
+  elif s[0] == '2':
+    if len(li) == 0:
+      print(-1)
+    else:
+      print(li.pop())
+  elif s[0] == '3':
+    print(len(li))
+  elif s[0] == '4':
+    if len(li) == 0:
+      print(1)
+    else:
+      print(0)
+  elif s[0] == '5':
+    if len(li) == 0:
+      print(-1)
+    else:
+      print(li[-1])
+      
