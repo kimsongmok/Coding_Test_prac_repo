eadd6a510dd1e2a40c89da5bff2dbcf517fdfec5
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2025-02-06 22:24:11 +0900

    [Silver I] Title: 트럭, Time: 100 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,21 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 n, w, L = map(int, input().split())
-a = [int(n) for n in input().split()]
-br = [0]*w
-ut = 0
+truck = list(map(int, input().split()))
 
+br = [0] * w
+ut = 0
 
 while br:
     ut +=1
     br.pop(0)
 
-    if a:
-        if sum(br) + a[0] > L:
+    if truck:
+        if sum(br) + truck[0] > L:
             br.append(0)
-        
         else:
-            br.append(a.pop(0))
-        
+            br.append(truck.pop(0))
+
 print(ut)
