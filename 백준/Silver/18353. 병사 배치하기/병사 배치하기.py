4186b140bfc228e07dab78a3b44fe6c5f0e62c25
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2025-02-12 19:44:40 +0900

    [Silver II] Title: 병사 배치하기, Time: 116 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,9 @@
+n = int(input())
+sol = list(map(int, input().split()))
+dp = [1] * n
+for i in range(1, n):
+    for j in range(0, i):
+        if sol[j] > sol[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n - max(dp))
