5558aaaf5f8593d6e978ba889bc98e0c42194b25
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2025-04-28 22:13:12 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 114200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,32 +1,32 @@
 from collections import deque
 
-n, m = map(int,input().split())
-maze = []
-for _ in range(n):
-    maze.append(list(map(int,input())))
+n, m =map(int, input().split())
+maze = [list(map(int, list(input()))) for _ in range(n)]
 
-def bfs(x,y):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
     q = deque()
-    q.append((x,y))
-    
+    q.append((x, y))
+
     while q:
-        x,y = q.popleft()
-        
+        x, y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            
+
             if maze[nx][ny] == 0:
                 continue
-            
+
             if maze[nx][ny] == 1:
                 maze[nx][ny] = maze[x][y] + 1
-                q.append((nx,ny))
-    print(maze[n-1][m-1])
+                q.append((nx, ny))
 
-bfs(0,0)
+    return maze[n-1][m-1]
+
+print(bfs(0,0))
