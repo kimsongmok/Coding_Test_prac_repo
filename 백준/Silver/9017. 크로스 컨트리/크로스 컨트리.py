0f564d3f2c9015b5d463fc24ddd551015f6c2128
KimSongMok <51540711+kimsongmok@users.noreply.github.com>
2025-08-16 21:59:44 +0900

    [Silver III] Title: 크로스 컨트리, Time: 120 ms, Memory: 110988 KB -BaekjoonHub

---
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -0,0 +1,40 @@
+t = int(input())
+
+for _ in range(t):
+  t_i = int(input())
+  team_num = list(map(int, input().split()))
+  team_num_set = set(sorted(team_num))
+  
+  cnt = {}
+  for i in team_num:
+    if i not in cnt:
+      cnt[i] = 0
+    cnt[i] += 1
+  
+  valid = []
+  for j in cnt:
+    if cnt[j] == 6:
+      valid.append(j)
+  
+  ranks = {}
+  rank = 1
+  for k in team_num:
+    if k in valid:
+      if k not in ranks:
+        ranks[k] = []
+      ranks[k].append(rank)
+      rank += 1
+  
+  best_team = -1
+  best_sum = 10**9
+  best_5th = 10**9
+
+  for l in valid:
+      s4 = ranks[l][0] + ranks[l][1] + ranks[l][2] + ranks[l][3]
+      r5 = ranks[l][4]
+      if s4 < best_sum or (s4 == best_sum and r5 < best_5th):
+          best_team = l
+          best_sum = s4
+          best_5th = r5
+
+  print(best_team)
